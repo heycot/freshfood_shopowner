ae962a2c918f3fda9963b8b197bde39fc8b9575e
heycot <nguyentu15061996@gmail.com>
2019-05-08 13:41:21 +0700

    add address, phone, birthday property to user_profile

--- freshfood_shopowner/Controller/SignUpController.swift
+++ freshfood_shopowner/Controller/SignUpController.swift
@@ -69,6 +69,9 @@ class SignUpController: UIViewController {
                         "name": self.nameTxt.text,
                         "email": self.emailTxt.text,
                         "password": self.passTxt.text,
+                        "phone": "",
+                        "address": "",
+                        "birthday": Date(),
                         "create_date": Date(),
                         "token": ""
                     ]) { err in
