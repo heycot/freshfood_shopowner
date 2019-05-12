2b74a59cb9edb36c89c07bb09bd99c22f6ae8f75
heycot <nguyentu15061996@gmail.com>
2019-05-12 18:30:57 +0700

    auto add new food cell

--- freshfood_shopowner/Controller/food/AddFoodController.swift
+++ freshfood_shopowner/Controller/food/AddFoodController.swift
@@ -12,16 +12,56 @@ class AddFoodController: UIViewController {
 
     @IBOutlet weak var notification: UILabel!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var notificationHeight: NSLayoutConstraint!
+    
+    var arrayCount = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        registerCell() 
+        setupView()
     }
     
+    func setupView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView =  UIView()
+    }
     
+    func registerCell() {
+        
+        tableView.register(UINib(nibName: CellIdentifier.newFood.rawValue, bundle: nil), forCellReuseIdentifier: CellIdentifier.newFood.rawValue)
+    }
     
-
+    @IBAction func newFoodPressed(_ sender: Any) {
+        
+        tableView.beginUpdates()
+        tableView.insertRows(at: [IndexPath(row: arrayCount - 1, section: 0)], with: .automatic)
+        tableView.endUpdates()
+        arrayCount += 1
+    }
+    
     @IBAction func doneBtnPressed(_ sender: Any) {
+        
     }
+    
+    
+}
+
+extension AddFoodController : UITableViewDelegate {
+    
+}
+
+extension AddFoodController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.newFood.rawValue, for: indexPath) as! NewFoodCell
+        
+        return cell
+    }
+    
     
 }
