aad063db556dec5e01f102f925d4f9810156dbcb
heycot <nguyentu15061996@gmail.com>
2019-05-12 13:57:46 +0700

    add account controllers file

---
+++ freshfood_shopowner/Controller/auth/EditInforController.swift
@@ -0,0 +1,30 @@
+//
+//  EditInforController.swift
+//  freshfood_shopowner
+//
+//  Created by Tu (Callie) T. NGUYEN on 5/12/19.
+//  Copyright © 2019 Tu (Callie) T. NGUYEN. All rights reserved.
+//
+
+import UIKit
+
+class EditInforController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
