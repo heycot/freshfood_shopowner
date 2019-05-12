faf6a51b30d739ee64cf78c774f3d06b1e73ed60
heycot <nguyentu15061996@gmail.com>
2019-05-12 17:12:30 +0700

    add addNewFood View Controller

---
+++ freshfood_shopowner/Controller/food/AddFoodController.swift
@@ -0,0 +1,25 @@
+//
+//  AddFoodController.swift
+//  freshfood_shopowner
+//
+//  Created by Tu (Callie) T. NGUYEN on 5/12/19.
+//  Copyright © 2019 Tu (Callie) T. NGUYEN. All rights reserved.
+//
+
+import UIKit
+
+class AddFoodController: UIViewController {
+
+    @IBOutlet weak var notification: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func doneBtnPressed(_ sender: Any) {
+    }
+    
+}
