46c79bf60431d49c7de0a5c94b7941f6ab186ddd
heycot <nguyentu15061996@gmail.com>
2019-05-26 16:41:39 +0700

    update channelView

--- freshfood_shopowner/Controller/chat/view/ChannelCell.swift
+++ freshfood_shopowner/Controller/chat/view/ChannelCell.swift
@@ -9,16 +9,26 @@
 import UIKit
 
 class ChannelCell: UITableViewCell {
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var channelImage: CustomImageView!
+    @IBOutlet weak var newMessage: UILabel!
+    
+    func updateView(channel: Channel) {
+        name.text = channel.name
+        name.setboldSystemFontOfSize(size: 18)
+        channelImage.displayImage(folderPath: channel.folder_image)
+        channelImage.setBorder(with: .white)
+    }
 }
