1afbbdd3b8cfea9f76b0781b934fd1afc93f4230
heycot <nguyentu15061996@gmail.com>
2019-05-26 16:35:52 +0700

    add ChannelViewController

---
+++ freshfood_shopowner/Controller/chat/view/ChannelCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChannelCell.swift
+//  freshfood_shopowner
+//
+//  Created by Tu (Callie) T. NGUYEN on 5/26/19.
+//  Copyright © 2019 Tu (Callie) T. NGUYEN. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
