f188e8f2d35c3d862d296d011402e06399df8a46
heycot <nguyentu15061996@gmail.com>
2019-05-26 16:39:43 +0700

    add databaseRepersentation model

---
+++ freshfood_shopowner/Model/chat/DatabaseRepresentation.swift
@@ -0,0 +1,13 @@
+//
+//  DatabaseRepresentation.swift
+//  freshfood_shopowner
+//
+//  Created by Tu (Callie) T. NGUYEN on 5/26/19.
+//  Copyright © 2019 Tu (Callie) T. NGUYEN. All rights reserved.
+//
+
+import Foundation
+
+protocol DatabaseRepresentation {
+    var representation: [String: Any] { get }
+}
