182514c1951121041802b28afcf658ad080b79af
heycot <nguyentu15061996@gmail.com>
2019-05-27 11:26:26 +0700

    add FindUserController

---
+++ freshfood_shopowner/Controller/chat/view/UserCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCell.swift
+//  freshfood_shopowner
+//
+//  Created by Tu (Callie) T. NGUYEN on 5/27/19.
+//  Copyright © 2019 Tu (Callie) T. NGUYEN. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
